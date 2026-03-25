ca941754a910692e87c9f72cbee97c49777fd105
conversion ignition--harmonic <conversion ignition--harmonic>
2026-03-25 14:13:34 +0100

    Update nav2_params.yaml and slam.launch.py

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -1,14 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+        # percorso del pacchetto nav2_bringup
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    fra2mo_nav_dir = FindPackageShare('fra2mo_navigation')
+    nav2_params_path = PathJoinSubstitution([fra2mo_nav_dir, 'config', 'nav2_params.yaml'])
+    
+    
+    
+    slam_node = Node(
             package='slam_toolbox',
             executable='sync_slam_toolbox_node',
             name='slam_toolbox',
             output='screen',
             parameters=[{'use_sim_time': True}],
-        ),
+        )
+    
+    navigation_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([nav2_bringup_dir, 'launch', 'navigation_launch.py'])
+            ),
+            launch_arguments={
+                'use_sim_time': 'true',  # Obbligatorio a 'true' se stai usando Gazebo
+                'params_file': nav2_params_path
+            }.items()
+        )
+    
+    return LaunchDescription([
+        slam_node,
+        navigation_launch
+
 
     ])
