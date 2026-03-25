d79069651714a08757a173a97cd7de784a47046d
conversion ignition--harmonic <conversion ignition--harmonic>
2026-03-25 13:55:43 +0100

    Fix: correzioni e aggiornamenti

--- launch/amcl.launch.py
+++ launch/amcl.launch.py
@@ -3,26 +3,40 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # percorso del pacchetto nav2_bringup
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
-    
+    fra2mo_nav_dir = FindPackageShare('fra2mo_navigation')
+    nav2_params_path = PathJoinSubstitution([fra2mo_nav_dir, 'config', 'nav2_params.yaml'])
     # Percorso della mappa
-    map_path = os.path.join(
-        get_package_share_directory('fra2mo_navigation'),
-        'maps',
-        'mappa2.yaml'
-    )
+    map_path = PathJoinSubstitution([fra2mo_nav_dir, 'maps', 'mappa_leonardo.yaml'])
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
+
+    navigation_launch = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(nav2_bringup_dir, 'launch', 'localization_launch.py')
+                PathJoinSubstitution([nav2_bringup_dir, 'launch', 'navigation_launch.py'])
+            ),
+            launch_arguments={
+                'use_sim_time': 'true',  # Obbligatorio a 'true' se stai usando Gazebo
+                'params_file': nav2_params_path
+            }.items()
+        )
+    
+
+    amcl_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                PathJoinSubstitution([nav2_bringup_dir, 'launch', 'localization_launch.py'])
             ),
             launch_arguments={
                 'use_sim_time': 'true',
-                'map': map_path
+                'map': map_path,
+                'params_file': nav2_params_path
             }.items()
         )
+    return LaunchDescription([
+        navigation_launch,
+        amcl_launch
     ])
